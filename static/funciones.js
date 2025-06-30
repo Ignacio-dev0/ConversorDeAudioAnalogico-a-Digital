147ef3639033ecbac5a48a318fc1150b7576ffcf
Ignacio-dev0 <nacho123benitez@gmail.com>
2025-06-29 21:00:36 -0300

    Limpiando el codigo

--- static/funciones.js
+++
@@ -1,8 +0,0 @@
-const grabarBtn = document.getElementById('grabar-btn');
-const audioElement = document.getElementById('audio');
-const resultadoElement = document.getElementById('resultado');
-
-console.log("llegue")
-grabarBtn.addEventListener('click', async () => {
-    
-});
