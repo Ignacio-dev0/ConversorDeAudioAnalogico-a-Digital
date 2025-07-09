5f4e7f479c7f207a3a54cb802ea679a224aa7dfa
Ignacio Benitez <162205026+Ignacio-dev0@users.noreply.github.com>
2025-07-09 19:17:56 -0300

    Update app.py

--- audio-conversor/backend/app.py
+++ audio-conversor/backend/app.py
@@ -5,6 +5,9 @@ import numpy as np
 from scipy.signal import resample
 import io
 from flask import send_file
+from pydub.utils import which
+
+AudioSegment.converter = which("ffmpeg")
 
 app = Flask(__name__)
 CORS(app)
